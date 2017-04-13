1ea5d1d6dfaff00205025ebb410f27b966383932
Andrea Navratilova <andy.navratilova@gmail.com>
2017-04-13 23:16:20 +0200

    basic recylerview, basic styles

--- app/src/main/java/pv239/brnorentalsapp/MainActivity.java
+++ app/src/main/java/pv239/brnorentalsapp/MainActivity.java
@@ -1,6 +1,6 @@
 package pv239.brnorentalsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,7 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
