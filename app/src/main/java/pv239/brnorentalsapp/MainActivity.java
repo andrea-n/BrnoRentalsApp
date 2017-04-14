f0cbd8a90afbbf469afa791989f6295320d00ac9
Andrea Navratilova <andy.navratilova@gmail.com>
2017-04-14 18:36:38 +0200

    settings to toolbar

--- app/src/main/java/pv239/brnorentalsapp/MainActivity.java
+++ app/src/main/java/pv239/brnorentalsapp/MainActivity.java
@@ -1,19 +1,25 @@
 package pv239.brnorentalsapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(myToolbar);
 
         List<Offer> offerList = new ArrayList<>();
         offerList.add(new Offer("Pronájem 2+1 Nový Lískovec"));
@@ -24,4 +30,11 @@ public class MainActivity extends Activity {
         recycler.setLayoutManager(new LinearLayoutManager(this));
         recycler.setAdapter(new MyAdapter(offerList));
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
