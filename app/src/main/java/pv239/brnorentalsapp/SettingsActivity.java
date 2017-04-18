bee99b67463534bf023446b08b0acc6216840fb1
Andrea Navratilova <andy.navratilova@gmail.com>
2017-04-18 08:02:03 +0200

    increment (opens new activity)

---
+++ app/src/main/java/pv239/brnorentalsapp/SettingsActivity.java
@@ -0,0 +1,22 @@
+package pv239.brnorentalsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.emptyToolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+}
